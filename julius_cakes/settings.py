aeedda4827066b21aee824f681ffa0a816a15cfc
ainebye <ainebyemartin0@gmail.com>
2026-07-22 20:15:43 +0300

    Critical Fix: Moved 'import os' to top of settings.py

--- julius_cakes/settings.py
+++ julius_cakes/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,8 +125,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-import os
-import dj_database_url
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
